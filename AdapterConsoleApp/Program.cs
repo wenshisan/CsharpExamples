a14e17319e3dc99b4ec16cef7b57b5defeb71f49
wenhisan <wenhisan@live.cn>
2019-12-03 16:57:56 +0800

    增加适配器模式

---
+++ AdapterConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace AdapterConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPlant target = new BJOrchard();
+            target.Plant();
+            target = new SHOrchard();
+            target.Plant();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    //种植接口
+    public interface IPlant
+    {
+        void Plant();
+    }
+
+    public class Orchard
+    {
+        public void Plant(string city, string fruit)
+        {
+            Console.WriteLine($"{city}果园种植{fruit}");
+        }
+    }
+
+    public class BJOrchard:Orchard,IPlant
+    {
+            public void Plant()
+            {
+                base.Plant("北京","苹果");
+            }
+    }
+
+    //对象适配器 上海果园
+    public class SHOrchard:IPlant
+    {
+        Orchard orchard = new Orchard();
+        public void Plant()
+        {
+            orchard.Plant("上海","橙子");
+        }
+    }
+}
