c69a7b13b7bf6e7058621d1b0b420a893a9616c5
wenhisan <wenhisan@live.cn>
2019-12-03 15:32:35 +0800

    增加单例模式

---
+++ SingletonConsoleApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace SingletonConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Helicopter helicopter = Helicopter.Instance;
+            helicopter.SprayInsecticide("上海");
+            helicopter.SprayInsecticide("北京");
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Helicopter
+    {
+        static Helicopter helicopter;
+
+        public static Helicopter Instance
+        {
+            get
+            {
+                if (helicopter == null)
+                {
+                    helicopter = new Helicopter();
+                }
+                return helicopter;
+            }
+        }
+
+        //直升机受保护的构造方法
+        protected Helicopter()
+        {
+
+
+        }
+        int insecticide = 100; //杀虫剂
+
+        //为指定城市喷洒杀虫剂
+        public void SprayInsecticide(string city)
+        {
+            insecticide -= 50;
+            Console.WriteLine($"为{city}果园喷洒农药");
+
+            if (insecticide == 0)
+            {
+                Console.WriteLine("杀虫剂已用完！");
+            }
+            else
+            {
+                Console.WriteLine($"杀虫剂还剩{insecticide}吨");
+            }
+        }
+    }
+}
